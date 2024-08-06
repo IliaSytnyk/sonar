f97c80ed0b4080a54e1e52e3a5b2b1112c50d243
IliaSytnyk <ilias@bandbsolution.com.ua>
2024-08-06 17:13:36 +0300

    first commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void test1() {
+        Main main = new Main();
+        assertEquals(main.test(), 4);
+    }
+}
